6c55418798e634b0e1454066b6ac64130098c875
Bryan Hickey <bryanjhickey@users.noreply.github.com>
2022-02-23 16:05:25 +1100

    feat: added seo component

--- web/src/pages/about.tsx
+++ web/src/pages/about.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '@/components/Layout';
-import Seo from '@/components/seo';
+import Seo from '@/components/Seo';
 
 export default function About({ data }) {
   const page = data.page;
